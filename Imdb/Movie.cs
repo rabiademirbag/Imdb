0612988e03b6b9d45bcb176d84592538e9bc5e66
rabiademirbag <rddemirbagg@gmail.com>
2025-05-29 16:23:40 +0300

    Proje dosyası ekle.

---
+++ Imdb/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imdb
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public double Imdb { get; set; }
+
+        public Movie(string name, double imdb)
+        {
+            Name = name;
+            Imdb = imdb;
+        }
+    }
+
+}
---
+++ Imdb/Program.cs
@@ -0,0 +1,64 @@
+﻿using Imdb;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Movie> movies = new List<Movie>();
+
+        string choice;
+
+        while (true)
+        {
+            Console.WriteLine("Film adı: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("IMDB puanı: ");
+            double imdb;
+            bool isValid = double.TryParse(Console.ReadLine(), out imdb);
+            while (!isValid || imdb < 0.0 || imdb > 10.0)
+            {
+                Console.WriteLine("Geçerli bir IMDB puanı giriniz :");
+                isValid = double.TryParse(Console.ReadLine(), out imdb);
+
+            }
+            Movie movie = new Movie(name, imdb);
+            movies.Add(movie);
+
+            Console.WriteLine("Başka bir film oluşturmak ister misiniz (evet/hayır):");
+            choice = Console.ReadLine();
+            if (choice != "evet")
+            {
+                break;
+
+            }
+        }
+
+
+        Console.WriteLine("Tüm Filmler:");
+        foreach (var i in movies)
+        {
+            Console.WriteLine(i.Name);
+        }
+        Console.WriteLine("IMDB 4-9 arası filmler: ");
+        foreach (var i in movies)
+        {
+            if (i.Imdb > 4 && i.Imdb < 9)
+            {
+                Console.WriteLine(i.Name);
+
+            }
+        }
+        Console.WriteLine("İsmi A ile başlayan filmler: ");
+        foreach (var i in movies)
+        {
+            if (i.Name.ToLower().StartsWith("a"))
+            {
+                Console.WriteLine("Film: " + i.Name + " Imdb puanı: " + i.Imdb);
+
+            }
+        }
+
+
+    }
+}
+        
